d7438c062b4548b72eab42dc2bc7e07f9f0d0ad4
Hung Viet <hung.viet.khuat@studium.uni-hamburg.de>
2024-12-18 00:59:48 +0100

    Bind FXML file with the backend

--- src/main/java/_2khuat/weatherapp/Controller/ViewController.java
+++ src/main/java/_2khuat/weatherapp/Controller/ViewController.java
@@ -1,5 +1,20 @@
 package _2khuat.weatherapp.Controller;
 
-public class ViewController {
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.input.MouseEvent;
 
+public class ViewController {
+    @FXML
+    Button okButton;
+    @FXML
+    PasswordField longitudeUserInput;
+    @FXML
+    PasswordField latitudeUserInput;
+
+    public void okButtonClickEvent(MouseEvent event){
+        System.out.println("Button clicked");
+    }
 }
