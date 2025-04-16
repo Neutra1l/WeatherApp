e415224c93b9eead9af5931e0d1b85fe4b078b03
2khuat <hung.viet.khuat@studium.uni-hamburg.de>
2025-04-16 14:17:51 +0200

    Reference GUI

--- src/main/java/_2khuat/weatherapp/Controller/ViewController.java
+++ src/main/java/_2khuat/weatherapp/Controller/ViewController.java
@@ -1,5 +1,6 @@
 package _2khuat.weatherapp.Controller;
 
+import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -16,5 +17,8 @@ public class ViewController {
 
     public void okButtonClickEvent(MouseEvent event){
         System.out.println("Button clicked");
+    }
+
+    public void handleClicks(ActionEvent actionEvent) {
     }
 }
