634be9c49df4c59f4afd05db64d9803104ced135
2khuat <hung.viet.khuat@studium.uni-hamburg.de>
2025-04-16 17:33:23 +0200

    First progress at implementing a functional GUI

--- src/main/java/_2khuat/weatherapp/Controller/HomeController.java
+++ src/main/java/_2khuat/weatherapp/Controller/HomeController.java
@@ -1,10 +1,15 @@
 package _2khuat.weatherapp.Controller;
 
 import _2khuat.weatherapp.Model.APIClient;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 import javafx.beans.property.StringProperty;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.SplitPane;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.input.InputMethodEvent;
 import javafx.scene.input.MouseEvent;
@@ -22,6 +27,14 @@ public class HomeController {
     Button btnPackages;
     @FXML
     TextField searchField;
+    @FXML
+    SplitPane weatherInfoDisplayPane;
+    @FXML
+    TextField cityName;
+    @FXML
+    TextArea tempDisplay;
+
+
     APIClient apiClient = APIClient.getApiClient();
 
     public void handleClick(MouseEvent mouseEvent) {
@@ -37,8 +50,14 @@ public class HomeController {
         else if(mouseEvent.getSource() == okButton){
             StringProperty query = searchField.textProperty();
             double[] coord = apiClient.getCoordinates(query.getValue());
-
+            JsonObject weatherData = apiClient.getWeatherData(coord);
+            cityName.setText(query.getValue());
+            JsonObject mainInfo = (JsonObject)weatherData.get("main");
+            double currentTemp = mainInfo.get("temp").getAsDouble();
+            tempDisplay.setText(currentTemp + " oC");
+            weatherInfoDisplayPane.setVisible(true);
         }
+
     }
 
     public void handleInputChange(InputMethodEvent inputMethodEvent) {
--- src/main/java/_2khuat/weatherapp/Model/APIClient.java
+++ src/main/java/_2khuat/weatherapp/Model/APIClient.java
@@ -5,7 +5,6 @@ import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
-import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
@@ -15,7 +14,6 @@ import java.net.URL;
 public class APIClient {
     private String _urlOpenWeather = "https://api.openweathermap.org/data/2.5/weather?";
     private String _urlGeoCoding = "http://api.openweathermap.org/geo/1.0/direct?";
-    private String weatherData;
     private final String _apiKey = "c7c6feca22d0f169d76feacae1e95ecd";
     private static APIClient singletonApiClient;
 
@@ -33,44 +31,40 @@ public class APIClient {
     public double[] getCoordinates(String query){
         String apiCall = _urlGeoCoding + "q=" + query + "&limit=1&appid=" + _apiKey;
         double[] coord = new double[2];
+        JsonElement geoCodingResponse = makeApiCall(apiCall);
+        JsonArray locationInfo = geoCodingResponse.getAsJsonArray();
+        JsonElement element = locationInfo.get(0);
+        JsonObject object = element.getAsJsonObject();
+        coord[0] = object.get("lat").getAsDouble();
+        coord[1] = object.get("lon").getAsDouble();
+        return coord;
+    }
+
+
+    public JsonObject getWeatherData(double[] coord) {
+        String apiCall = _urlOpenWeather + "lat=" + coord[0] + "&" + "lon=" + coord[1] + "&appid=" + _apiKey;
+        JsonElement weatherData = makeApiCall(apiCall);
+        return weatherData.getAsJsonObject();
+    }
+
+    private JsonElement makeApiCall(String apiCall){
+        JsonElement jsonElement = new JsonElement() {
+            @Override
+            public JsonElement deepCopy() {
+                return null;
+            }
+        };
         try {
             URL url = new URL(apiCall);
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
             conn.setRequestMethod("GET");
             InputStream inputStream = conn.getInputStream();
             Reader reader = new InputStreamReader(inputStream);
-            JsonElement jsonElement = JsonParser.parseReader(reader);
-            JsonArray arr = jsonElement.getAsJsonArray();
-            JsonElement element = arr.get(0);
-            JsonObject object = element.getAsJsonObject();
-            coord[0] = object.get("lat").getAsDouble();
-            coord[1] = object.get("lon").getAsDouble();
-        } catch (Exception e) {
+            jsonElement = JsonParser.parseReader(reader);
+        }
+        catch(Exception e){
             e.printStackTrace();
         }
-        return coord;
-    }
-
-    private String getWeatherData(String apiUrl) {
-        StringBuilder result = new StringBuilder();
-        try {
-            URL url = new URL(apiUrl);
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setRequestMethod("GET");
-
-            BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-            String line;
-            while ((line = reader.readLine()) != null) {
-                result.append(line);
-            }
-            reader.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return result.toString();
-    }
-
-    public JsonObject parseWeatherData(String jsonData) {
-        return JsonParser.parseString(jsonData).getAsJsonObject();
+        return jsonElement;
     }
 }
